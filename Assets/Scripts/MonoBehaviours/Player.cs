66a1604d788c3166271872adf6887bd394edee06
Osmar Villegas <osmarvm25@gmail.com>
2024-10-28 19:48:13 -0600

    Documento 4 terminado
    El documento 4 se ha terminado.

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Player : Character
@@ -8,7 +11,34 @@ public class Player : Character
     {
         if (collision.gameObject.CompareTag("CanBePickedUp"))
         {
-            collision.gameObject.SetActive(false);
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+            if (hitObject != null)
+            {
+                print("Nombre: " + hitObject.objectName);
+
+
+                switch (hitObject.itemType)
+                {
+                    case Item.ItemType.COIN:
+                        break;
+                    case Item.ItemType.HEALTH:
+                        AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+
+                }
+
+                collision.gameObject.SetActive(false);
+            }
         }
+
     }
+
+    public void AdjustHitPoints(int amount)
+    {
+        hitPoints = hitPoints + amount;
+        print("Ajustando Puntos: " + amount + ". Nuevo Valor: " + hitPoints);
+    }
+
 }
